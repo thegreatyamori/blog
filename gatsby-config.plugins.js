fbf1c05a541edfa4e4eb845c9bcd453f7431b080
thegreatyamori <jersonmorocho16@gmail.com>
2020-06-15 07:44:24 -0500

    Actualización NPM + Analytics

--- gatsby-config.plugins.js
+++ gatsby-config.plugins.js
@@ -2,62 +2,70 @@ const config = require('./config')
 const feedPlugin = require('./gatsby-config.plugins.feed')
 
 module.exports = [
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-less',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: 'UA-151033495-1',
+      head: true,
+      anonymize: true,
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'Jerson Alexander Blog',
-        short_name: 'Jerson Alexander',
-        start_url: '/',
-        background_color: '#0C2744',
-        theme_color: '#0C2744',
-        display: 'standalone',
-        icon: 'src/images/icon.png', // This path is relative to the root of the site.
-        legacy: true, // this will add apple-touch-icon links to <head>. Required for versions prior to iOS 11.3.
-      },
+  },
+  'gatsby-plugin-react-helmet',
+  'gatsby-transformer-sharp',
+  'gatsby-plugin-sharp',
+  'gatsby-plugin-less',
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/content`,
-      },
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: 'Jerson Alexander Blog',
+      short_name: 'Jerson Alexander',
+      start_url: '/',
+      background_color: '#0C2744',
+      theme_color: '#0C2744',
+      display: 'standalone',
+      icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      legacy: true, // this will add apple-touch-icon links to <head>. Required for versions prior to iOS 11.3.
     },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1000,
-              quality: 80,
-              showCaptions: true,
-              linkImagesToOriginal: false
-            },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdown-pages`,
+      path: `${__dirname}/content`,
+    },
+  },
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 1000,
+            quality: 80,
+            showCaptions: true,
+            linkImagesToOriginal: false,
           },
-          'gatsby-remark-prismjs'
-        ]
-      }
+        },
+        'gatsby-remark-prismjs',
+      ],
     },
-    {
-      resolve: 'gatsby-plugin-i18n',
-      options: {
-        langKeyDefault: config.defaultLanguage,
-        useLangKeyLayout: false
-      }
+  },
+  {
+    resolve: 'gatsby-plugin-i18n',
+    options: {
+      langKeyDefault: config.defaultLanguage,
+      useLangKeyLayout: false,
     },
-    feedPlugin,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-robots-txt`
+  },
+  feedPlugin,
+  `gatsby-plugin-sitemap`,
+  `gatsby-plugin-robots-txt`,
 ]
