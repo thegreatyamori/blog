5d9093e42e143d990c93dbd4898690aa8db3a749
thegr <tod.jerson.morocho@gmail.com>
2021-11-28 16:04:20 -0500

    feat: update description

--- src/templates/post/author/author.js
+++ src/templates/post/author/author.js
@@ -5,23 +5,24 @@ import Img from 'gatsby-image'
 import style from './author.module.less'
 import Config from '../../../../config'
 
+const jsEcuador = <a
+  href={Config.footer.jsEcuador.telegram}
+  target="_blank"
+  rel="nofollow noopener noreferrer"
+>
+  Javascript Ecuador
+</a>
+
 const Author = ({ img }) => (
   <div className={style.container}>
     <div className={style.avatar}>
       <Img fluid={img} className={style.rounded} title="Jerson" alt="Jerson" />
     </div>
     <p className={style.description}>
-      <b>jerson alexander</b> es estudiante de Ingenieria en Software en
-      Ecuador, disfruta mucho de la auto-educación, programación, música, libros, manga, arte y cultura
-      general, además es seguidor de Jesucristo y lector de la biblia. Actualmente es miembro en la comunidad de&nbsp;
-      <a
-        href={Config.footer.jsEcuador.telegram}
-        target="_blank"
-        rel="nofollow noopener noreferrer"
-      >
-        jsEcuador
-      </a>
-      .
+      <b>Sobre mí</b> Soy Ingeniero de Software en ioet, Inc. Disfruto mucho de
+      la auto-educación, programación, música, libros, manga, arte y cultura general.
+      Práctico ciclismo y basketball. Soy seguidor de Jesús y me encanta la biblia.
+      Actualmente formo parte de la comunidad de {jsEcuador}.
     </p>
   </div>
 )
